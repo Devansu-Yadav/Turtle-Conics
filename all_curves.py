dc2d04e0c047abc443041167de033ba70069b983
Devansu Yadav <devansuyadav@gmail.com>
2020-05-27 03:58:39 -0700

    Update all_curves.py

--- all_curves.py
+++ all_curves.py
@@ -94,4 +94,42 @@ def hyperbola(a, b, h=None, k=None):
             else:
                 myturtle.down()
             myturtle.setposition(h+b*math.tan(i/10), k+a*(1/math.cos(i/10)))
+            
+def cardioid(a, orientation, h=None, k=None, angle=None, angle_unit=None):
+    myturtle = turtle.Turtle()
+    if h is None:
+        h = 0
+    if k is None:
+        k = 0
+    # Angle unit can be degree or radian
+
+    if angle is None:
+        angle = 360
+        converted_angle = angle * 0.875
+
+    if angle_unit == 'd' or angle_unit is None:
+        converted_angle = angle * 0.875
+
+    # We are multiplying by 0.875 because for making a complete cardioid we are plotting 315 pts according
+    # to our parametric angle value
+
+    elif angle_unit == "r":
+        converted_angle = (angle * 0.875 * (180/math.pi))
+    # Converting radian to degrees.
+
+    if orientation == 'x':
+        for i in range(int(converted_angle)+1):
+            if i == 0:
+                myturtle.up()
+            else:
+                myturtle.down()
+            myturtle.setposition(h+a*math.cos(i/50)*(1-math.cos(i/50)), k+a*math.sin(i/50)*(1-math.cos(i/50)))
+
+    elif orientation == 'y':
+        for i in range(int(converted_angle)+1):
+            if i == 0:
+                myturtle.up()
+            else:
+                myturtle.down()
+            myturtle.setposition(h+a*math.sin(i/50)*(1-math.cos(i/50)), k+a*math.cos(i/50)*(1-math.cos(i/50)))
 

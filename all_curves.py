f2df9eea178dc1a109618dd79564bde8e758d4cc
Devansu Yadav <devansuyadav@gmail.com>
2020-06-06 02:17:32 -0700

    Update all_curves.py

--- all_curves.py
+++ all_curves.py
@@ -132,4 +132,51 @@ def cardioid(a, orientation, h=None, k=None, angle=None, angle_unit=None):
             else:
                 myturtle.down()
             myturtle.setposition(h+a*math.sin(i/50)*(1-math.cos(i/50)), k+a*math.cos(i/50)*(1-math.cos(i/50)))
+            
+            
+def heart(a, b, orientation, h=None, k=None, angle=None, angle_unit=None):
+    myturtle = turtle.Turtle()
+
+    if h is None:
+        h = 0
+    if k is None:
+        k = 0
+    # Angle unit can be degree or radian
+
+    if angle is None:
+        angle = 360
+        converted_angle = angle * 0.875
+
+    if angle_unit == 'd' or angle_unit is None:
+        converted_angle = angle * 0.875
+
+    # We are multiplying by 0.875 because for making a complete cardioid we are plotting 315 pts according
+    # to our parametric angle value
+
+    elif angle_unit == "r":
+        converted_angle = (angle * 0.875 * (180/math.pi))
+    # Converting radian to degrees.
+
+    if orientation == 'y':
+        i = 0
+        while i <= converted_angle:
+            if i == 0:
+                myturtle.up()
+            else:
+                myturtle.down()
+            myturtle.setposition(h+a*(16*math.pow(math.sin(i/50), 3)), k+b*(13*math.cos(i/50)-5*math.cos(2*(i/50))-2*math.cos(3*(i/50))
+                                 - math.cos(4*(i/50))))
+            i += 1
+
+    elif orientation == 'x':
+        i = 0
+        while i <= converted_angle:
+            if i == 0:
+                myturtle.up()
+            else:
+                myturtle.down()
+            myturtle.setposition(k+b*(13*math.cos(i/50)-5*math.cos(2*(i/50))-2*math.cos(3*(i/50))
+                                 - math.cos(4*(i/50))), h+a*(16*math.pow(math.sin(i/50), 3)))
+            i += 1
+
 
